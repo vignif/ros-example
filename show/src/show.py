213c93bc074ca30abbff9c3c8cde35bba3cf0c36
francesco <vignif@gmail.com>
2021-05-01 18:11:06 +0200

    Adjust show  pkg

--- show/src/show.py
+++ show/src/show.py
@@ -1,33 +1,50 @@
+#!/usr/bin/env python
+
 import plotly.graph_objects as go
 import pandas as pd
 import sqlite3
 import os
-import rospy
 import rospkg
+from std_msgs.msg import Empty
+import rospy
 
 
-try:
-    rospack = rospkg.RosPack()
-    path = rospack.get_path("db_handler")
-    db_path = path + "/test.db"
-    con = sqlite3.connect(db_path)
-    sql = "SELECT * FROM cities"
-    df = pd.read_sql_query(sql, con)
+class Show:
+    def __init__(self):
+        rospy.init_node("Show")
+        self.df = None
+        self.sub = rospy.Subscriber("render_cities", Empty, self.get_cities)
 
-except Exception as e:
-    rospy.logerr("Can't access database")
+    def get_cities(self, data):
+        try:
+            rospack = rospkg.RosPack()
+            path = rospack.get_path("db_handler")
+            db_path = path + "/test.db"
+            con = sqlite3.connect(db_path)
+            sql = "SELECT * FROM cities"
+            self.df = pd.read_sql_query(sql, con)
 
-fig = go.Figure(
-    data=go.Scattergeo(
-        lon=df["longitude"],
-        lat=df["latitude"],
-        text=df["name"],
-        mode="markers",
-    )
-)
+        except Exception as e:
+            rospy.logerr("Can't access database")
+        self.render()
 
-fig.update_layout(
-    title='City in database of ros example<br>(Hover for city names)<br> <a href="https://github.com/vignif/ros-example">link</a>',
-    geo_scope="europe",
-)
-fig.show()
+    def render(self):
+        fig = go.Figure(
+            data=go.Scattergeo(
+                lon=self.df["longitude"],
+                lat=self.df["latitude"],
+                text=self.df["name"],
+                mode="markers",
+            )
+        )
+
+        fig.update_layout(
+            title='City in database of ros example<br>(Hover for city names)<br> <a href="https://github.com/vignif/ros-example">link</a>',
+            geo_scope="europe",
+        )
+        fig.show()
+
+
+if __name__ == "__main__":
+    Show()
+    rospy.spin()
